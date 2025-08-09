9a8b295de6ace03a40bb5d1b0371afb10f842a7c
KB Master <proofofrandomness@gmail.com>
2025-08-09 11:07:17 +0300

    WIP

--- cache/Events/dist/onRequest/index.js
+++ cache/Events/dist/onRequest/index.js
@@ -47,129 +47,120 @@ __nccwpck_require__.d(__webpack_exports__, {
 });
 
 ;// CONCATENATED MODULE: ./actions.js
+const getActions = () => [
+    [/\/?googleSearch\("(.*)"\)/, async (match) => {
+        const q = match[1];
+        try {
+            const response = await openkbs.googleSearch(q, {});
+            const data = response?.map(({ title, link, snippet, pagemap }) => ({
+                title, link, snippet, image: pagemap?.metatags?.[0]?.["og:image"]
+            }));
+            return { data };
+        } catch (e) {
+            return { error: e.message };
+        }
+    }],
 
-const getActions = (meta) => {
-    return [
+    [/\/?webpageToText\("(.*)"\)/, async (match) => {
+        try {
+            let response = await openkbs.webpageToText(match[1]);
+            if(!response?.url) return { data: { error: "Unable to read website" } };
+            return { data: response };
+        } catch (e) {
+            return { error: e.response?.data || e };
+        }
+    }],
 
-        [/\/?textToImage\("(.*)"\)/, async (match) => {
-            const response = await openkbs.textToImage(match[1], { serviceId: 'stability.sd3Medium' });
-            const imageSrc = `data:${response.ContentType};base64,${response.base64Data}`;
-            return { type: 'SAVED_CHAT_IMAGE', imageSrc, ...meta };
-        }],
+    [/\/?documentToText\("(.*)"\)/, async (match) => {
+        try {
+            let response = await openkbs.documentToText(match[1]);
+            return { data: response };
+        } catch (e) {
+            return { error: e.response.data };
+        }
+    }],
+
+    [/\/?imageToText\("(.*)"\)/, async (match) => {
+        try {
+            let response = await openkbs.imageToText(match[1]);
+
+            if (response?.detections?.[0]?.txt) {
+                response = { detections: response?.detections?.[0]?.txt };
+            }
+
+            return { data: response };
+        } catch (e) {
+            return { error: e.response.data };
+        }
+    }],
+
+    [/\/?textToSpeech\("(.*)"\s*,\s*"(.*)"\)/, async (match) => {
+        try {
+            const response = await openkbs.textToSpeech(match[2], {
+                languageCode: match[1]
+            });
+            return { data: response };
+        } catch (e) {
+            return { error: e.response.data };
+        }
+    }],
+];
+
+;// CONCATENATED MODULE: ./handler.js
 
 
-        [/\/?googleSearch\("(.*)"\)/, async (match) => {
-            const q = match[1];
-            const searchParams = match[2] && JSON.parse(match[2]) || {};
-            try {
-                const noSecretsProvided = '{{secrets.googlesearch_api_key}}'.includes('secrets.googlesearch_api_key');
+const backendHandler = async (event) => {
+    const lastMessage = event.payload.messages[event.payload.messages.length - 1];
+    const actions = getActions();
 
-                const params = {
-                    q,
-                    ...searchParams,
-                    ...(noSecretsProvided ? {} : { key: '{{secrets.googlesearch_api_key}}', cx: '{{secrets.googlesearch_engine_id}}' }),
+    const matchingActions = actions.reduce((acc, [regex, action]) => {
+        const matches = [...lastMessage.content.matchAll(new RegExp(regex, 'g'))];
+        matches.forEach(match => {
+            acc.push(action(match, event));
+        });
+        return acc;
+    }, []);
+
+    const reachedMessageLimit = event?.payload?.messages?.length > 60;
+
+    if (matchingActions.length > 0) {
+        try {
+            const results = await Promise.all(matchingActions);
+            
+            const isOnlyJobCompletion = results.length === 1 && 
+                (results[0]?.type === 'JOB_COMPLETED' || results[0]?.type === 'JOB_FAILED');
+            
+            const meta = {
+                _meta_actions: (reachedMessageLimit || isOnlyJobCompletion) ? [] : ["REQUEST_CHAT_MODEL"]
+            };
+
+            if (results?.[0]?.data?.some?.(o => o?.type === 'image_url')) {
+                return {
+                    ...results[0],
+                    ...meta
                 };
-
-                const response = noSecretsProvided
-                    ? await openkbs.googleSearch(params.q, params)
-                    : (await axios.get('https://www.googleapis.com/customsearch/v1', { params }))?.data?.items;
-
-                const data = response?.map(({ title, link, snippet, pagemap }) => ({
-                    title,
-                    link,
-                    snippet,
-                    image: pagemap?.metatags?.[0]?.["og:image"]
-                }));
-
-                return { data, ...meta };
-
-            } catch (e) {
-                return { error: e.response.data, ...meta };
             }
-        }],
 
+            return {
+                type: 'RESPONSE',
+                data: results,
+                ...meta
+            };
+        } catch (error) {
+            return {
+                type: 'ERROR',
+                error: error.message,
+                _meta_actions: reachedMessageLimit ? [] : ["REQUEST_CHAT_MODEL"]
+            };
+        }
+    }
 
-
-        [/\/?webpageToText\("(.*)"\)/, async (match) => {
-            try {
-                let response = await openkbs.webpageToText(match[1]);
-
-                // limit output length
-                if (response?.content?.length > 5000) {
-                    response.content = response.content.substring(0, 5000);
-                }
-
-                return { data: response, ...meta };
-            } catch (e) {
-                return { error: e.response.data, ...meta };
-            }
-        }],
-
-
-
-        [/\/?documentToText\("(.*)"\)/, async (match) => {
-            try {
-                let response = await openkbs.documentToText(match[1]);
-
-                // limit output length
-                if (response?.text?.length > 5000) {
-                    response.text = response.text.substring(0, 5000);
-                }
-
-                return { data: response, ...meta };
-            } catch (e) {
-                return { error: e.response.data, ...meta };
-            }
-        }],
-
-
-
-        [/\/?imageToText\("(.*)"\)/, async (match) => {
-            try {
-                let response = await openkbs.imageToText(match[1]);
-
-                if (response?.detections?.[0]?.txt) {
-                    response = { detections: response?.detections?.[0]?.txt };
-                }
-
-                return { data: response, ...meta };
-            } catch (e) {
-                return { error: e.response.data, ...meta };
-            }
-        }],
-
-
-
-        [/\/?textToSpeech\("(.*)"\s*,\s*"(.*)"\)/, async (match) => {
-            try {
-                const response = await openkbs.textToSpeech(match[2], {
-                    languageCode: match[1]
-                });
-                return { data: response, ...meta };
-            } catch (e) {
-                return { error: e.response.data, ...meta };
-            }
-        }],
-
-
-    ];
-}
-
+    return { type: 'CONTINUE' };
+};
 ;// CONCATENATED MODULE: ./onRequest.js
 
 
-
-const handler = async (event) => {
-    const actions = getActions({});
-
-    for (let [regex, action] of actions) {
-        const lastMessage = event.payload.messages[event.payload.messages.length - 1].content;        
-        const match = lastMessage?.match(regex);        
-        if (match) return await action(match);            
-    }
-
-    return { type: 'CONTINUE' }
-};
+const handler = async (event) => backendHandler(event)
 module.exports = __webpack_exports__;
 /******/ })()
 ;
